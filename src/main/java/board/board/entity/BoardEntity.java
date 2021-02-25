1b0f613dad52f56f5c5299aaf3ab54bdf986bda5
wcjeong <wcjeong@hist.co.kr>
2021-02-25 17:20:37 +0900

    추가 수정

--- src/main/java/board/board/entity/BoardEntity.java
+++ src/main/java/board/board/entity/BoardEntity.java
@@ -1,9 +1,19 @@
 package board.board.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Collection;
 
@@ -29,10 +39,12 @@ public class BoardEntity {
     private String creatorId;
 
     @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdDatetime = LocalDateTime.now();
 
     private String updatorId;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime updatedDatetime;
 
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
--- src/main/java/board/board/repository/JpaBoardRepository.java
+++ src/main/java/board/board/repository/JpaBoardRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 public interface JpaBoardRepository extends CrudRepository<BoardEntity, Integer> {
     List<BoardEntity> findAllByOrderByBoardIdxDesc();
 
-    @Query("SELECT file FROM BoardEntity file WHERE board_idx = :boardIdx AND idx = :idx")
+    @Query("SELECT file FROM BoardFileEntity file WHERE board_idx = :boardIdx AND idx = :idx")
     BoardFileEntity findBoardFile(@Param("boardIdx") int boardIdx, @Param("idx") int idx);
 }
